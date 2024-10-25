bef7ce45c5944baec188018bc809517ecbd08eb0
MicaelSv <micaelsilva012@hotmail.com>
2024-10-25 20:38:57 -0300

    rota

--- src/components/homePage/LoginModal.jsx
+++ src/components/homePage/LoginModal.jsx
@@ -14,7 +14,7 @@ function LoginModal({ onClose }) {
     const loginData = { email, password };
 
     try {
-      const response = await fetch('/api/login', {
+      const response = await fetch('https://api-accessable.vercel.app/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
