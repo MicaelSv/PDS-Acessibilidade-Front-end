9e595dd69775de85a990c306b5d5a7dbaa7d1b7f
MicaelSv <micaelsilva012@hotmail.com>
2024-10-30 11:08:50 -0300

    erro-de-rota2

--- src/components/empresa/MinhasVagas.jsx
+++ src/components/empresa/MinhasVagas.jsx
@@ -14,8 +14,7 @@ function MinhasVagas() {
   const [candidatos, setCandidatos] = useState([]); // Novo estado para candidatos
 
   const handleVerCurriculo = (usuarioId) => {
-    const baseUrl = window.location.origin; // Pega a URL base da aplicação
-    window.open(`${baseUrl}/CurriculoCandidato/${usuarioId}`, '_blank');
+    window.open(`/CurriculoCandidato/${usuarioId}`, '_blank');
   };
 
   const statusOptions = {
